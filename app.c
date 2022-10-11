5f820e67122cd7bc98771fbb9c95e73571340608
Ka <69207529+Ka-Konata@users.noreply.github.com>
2022-10-11 17:45:53 -0300

    implementando json, teste 1

--- app.c
+++ app.c
@@ -1,7 +1,25 @@
+// Inserindo bibliotecas
 #include <stdio.h>
 #include <stdlib.h>
+//#include <json.c/json.h>
 
+// Definindo Funções
+// [...]
+
+// Definindo constantes
+#define BD_path "bd/teste.json"
+#define BD_size 1024
+
+// Código Principal
 int main(void) {
-    printf("Hello World\n");
+    char buffer[BD_size] = {0};
+
+    FILE * f = fopen(BD_path, "r");
+    fread(buffer, BD_size, 1, f);
+
+
+    printf("Arquivo aberto/criado com sucesso!\n"); //DEBUG
+    printf("%s\n", buffer); //DEBUG
+    fclose(f);
     system("pause");
 } 
