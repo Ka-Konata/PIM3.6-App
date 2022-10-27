00ecad47ea98ccab83a83fe39a432525f53d1e9c
Ka <69207529+Ka-Konata@users.noreply.github.com>
2022-10-27 00:08:41 -0300

    aaa

--- app.c
+++ app.c
@@ -6,18 +6,21 @@
 //#include <json.c/json.h>
 
 // Definindo constantes
-#define BD_alunos "bd/bd_alunos.json"
-#define BD_size 1024
+#define BD_ALUNOS_PATH "bd/bd_alunos.json"
+#define BD_SIZE 1024
 
-char bd_alunos[BD_size] = {0};
+// Para armazenar os arquivos 
+char bd_alunos[BD_SIZE] = {0};
+// Para armazenar as informações dos arquivos em formato de arrays
+char bd_alunos_arr;
 
 // Código Principal
 int main(void) {
 	//setlocale(LC_ALL, );
 	char get_bd_as_a_vector(char *bd);
 
-    FILE * f = fopen(BD_alunos, "r");
-    fread(bd_alunos, BD_size, 1, f);
+    FILE * f = fopen(BD_ALUNOS_PATH, "r");
+    fread(bd_alunos, BD_SIZE, 1, f);
 	get_bd_as_a_vector(bd_alunos);
 
     //printf("Arquivo aberto/criado com sucesso!\n"); //DEBUG
@@ -35,7 +38,7 @@ char get_bd_as_a_vector(char *bd) {
 	char *bd_clear = (char *)malloc(strlen(bd)+1);
 	char index[5], *new_bd[500][4][200], table[4][200];
 	int run = 1, count = 0;
-	int c1, c2, c3;
+	int c1, c2;
 
 	strcpy(bd_clear, bd);
 	while(run == 1) {
@@ -43,13 +46,18 @@ char get_bd_as_a_vector(char *bd) {
 
 		sprintf(index, "%d", count);
 		strcat(index, "\":");
-		bd_clear = 4 + strstr(bd_clear, index);
+
+		if (count < 10) bd_clear = 4 + strstr(bd_clear, index);
+		else if (count < 100 ) bd_clear = 5 + strstr(bd_clear, index);
+		else bd_clear = 6 + strstr(bd_clear, index);
+		
 		strcpy(bd_clear_actual, bd_clear);
 		bd_clear_actual = strtok(bd_clear_actual, "]");
 
 		char separator[] = ",";
 		char *bd_table = strtok(bd_clear_actual, separator);
 		int i = 0;
+
 		while(bd_table != NULL)
 		{
 			strcpy(table[i], bd_table);
@@ -58,15 +66,12 @@ char get_bd_as_a_vector(char *bd) {
 		}
 
 		for(c1 = 0; c1 < (int) (sizeof(table) / sizeof(table[0])); c1++) {
-			for(c2 = 0; c2 < (int) (sizeof(table[c1]) / sizeof(table[c1][0])); c2++) {
-				
-				//strcpy(new_bd[count][c1], table[c1]);
-				//printf("%s\n", new_bd[count][c1]);
-			}
+			strcpy(new_bd[count][c1], table[c1]);
+			printf("%s\n", new_bd[count][c1]);
 		}
 
 		printf("index = %s | table = [%s, %s, %s, %s]\n", index, table[0], table[1], table[2], table[3]);
-		printf("bd_clear = %s\n\n", bd_clear);
+		printf("bd_clear = %s\n", bd_clear);
   
 		count++;
 		if(strstr(bd_clear, "\":") == NULL) break;
